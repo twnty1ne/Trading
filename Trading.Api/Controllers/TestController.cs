f0161700b2b6f07fdb66fce20db4276c98a73ef1
twnty1ne <nnstuff123@gmail.com>
2022-08-16 14:36:36 +0300

    primary api setup

---
+++ Trading.Api/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trading.Api.Controllers
+{
+    [ApiController]
+    [Route("test")]
+    public class TestController : ControllerBase
+    {
+        public Task<IActionResult> TestMethod() 
+        {
+            return Task.FromResult<IActionResult>(Ok());
+        }
+    }
+}
