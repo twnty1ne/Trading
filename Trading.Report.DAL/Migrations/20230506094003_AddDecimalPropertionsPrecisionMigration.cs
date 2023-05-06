3e07d89c73426273ca840f48d50be6fa464268ca
twnty1ne <nnstuff123@gmail.com>
2023-05-06 18:57:05 +0300

    TRAD-55 add decimal precision to EF

---
+++ Trading.Report.DAL/Migrations/20230506094003_AddDecimalPropertionsPrecisionMigration.cs
@@ -0,0 +1,313 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Trading.Report.DAL.Migrations
+{
+    public partial class AddDecimalPropertionsPrecisionMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Volume",
+                table: "TradeCandles",
+                type: "decimal(18,6)",
+                precision: 18,
+                scale: 6,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Open",
+                table: "TradeCandles",
+                type: "decimal(18,6)",
+                precision: 18,
+                scale: 6,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Low",
+                table: "TradeCandles",
+                type: "decimal(18,6)",
+                precision: 18,
+                scale: 6,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "High",
+                table: "TradeCandles",
+                type: "decimal(18,6)",
+                precision: 18,
+                scale: 6,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Close",
+                table: "TradeCandles",
+                type: "decimal(18,6)",
+                precision: 18,
+                scale: 6,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "UnrealizedPnL",
+                table: "Positions",
+                type: "decimal(18,6)",
+                precision: 18,
+                scale: 6,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TakeProfit",
+                table: "Positions",
+                type: "decimal(18,6)",
+                precision: 18,
+                scale: 6,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "StopLoss",
+                table: "Positions",
+                type: "decimal(18,6)",
+                precision: 18,
+                scale: 6,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Size",
+                table: "Positions",
+                type: "decimal(18,6)",
+                precision: 18,
+                scale: 6,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "RealizedPnl",
+                table: "Positions",
+                type: "decimal(18,6)",
+                precision: 18,
+                scale: 6,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ROE",
+                table: "Positions",
+                type: "decimal(18,6)",
+                precision: 18,
+                scale: 6,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "InitialMargin",
+                table: "Positions",
+                type: "decimal(18,6)",
+                precision: 18,
+                scale: 6,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "IMR",
+                table: "Positions",
+                type: "decimal(18,6)",
+                precision: 18,
+                scale: 6,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "EntryPrice",
+                table: "Positions",
+                type: "decimal(18,6)",
+                precision: 18,
+                scale: 6,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "CurrentPrice",
+                table: "Positions",
+                type: "decimal(18,6)",
+                precision: 18,
+                scale: 6,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Volume",
+                table: "TradeCandles",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,6)",
+                oldPrecision: 18,
+                oldScale: 6);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Open",
+                table: "TradeCandles",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,6)",
+                oldPrecision: 18,
+                oldScale: 6);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Low",
+                table: "TradeCandles",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,6)",
+                oldPrecision: 18,
+                oldScale: 6);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "High",
+                table: "TradeCandles",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,6)",
+                oldPrecision: 18,
+                oldScale: 6);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Close",
+                table: "TradeCandles",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,6)",
+                oldPrecision: 18,
+                oldScale: 6);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "UnrealizedPnL",
+                table: "Positions",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,6)",
+                oldPrecision: 18,
+                oldScale: 6);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TakeProfit",
+                table: "Positions",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,6)",
+                oldPrecision: 18,
+                oldScale: 6);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "StopLoss",
+                table: "Positions",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,6)",
+                oldPrecision: 18,
+                oldScale: 6);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Size",
+                table: "Positions",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,6)",
+                oldPrecision: 18,
+                oldScale: 6);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "RealizedPnl",
+                table: "Positions",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,6)",
+                oldPrecision: 18,
+                oldScale: 6);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ROE",
+                table: "Positions",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,6)",
+                oldPrecision: 18,
+                oldScale: 6);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "InitialMargin",
+                table: "Positions",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,6)",
+                oldPrecision: 18,
+                oldScale: 6);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "IMR",
+                table: "Positions",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,6)",
+                oldPrecision: 18,
+                oldScale: 6);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "EntryPrice",
+                table: "Positions",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,6)",
+                oldPrecision: 18,
+                oldScale: 6);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "CurrentPrice",
+                table: "Positions",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,6)",
+                oldPrecision: 18,
+                oldScale: 6);
+        }
+    }
+}
